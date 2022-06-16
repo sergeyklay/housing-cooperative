7fc319b27fc9e3cd0920c7ae27f548fd921e0efc
Serghei Iakovlev <egrep@protonmail.ch>
2022-06-17 01:56:15 +0200

    Add license

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+# This file is part of the Housing Cooperative.
+#
+# Copyright (c) 2022 Serghei Iakovlev
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
 ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 SHELL    := $(shell which bash)
 
--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# This file is part of the Housing Cooperative.
+#
+# Copyright (c) 2022 Serghei Iakovlev
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
